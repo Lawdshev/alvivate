ba7892eecdf2259e1e19f81ff7ec6390f3c9f4fc
rash1234567 <imamrasheedatahmad1993@gmail.com>
2022-11-13 21:38:56 -0800

    cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-import Card from 'react-bootstrap/Card';
+import Star3 from '../utilities/images/3Star.png'
 
 
 export default function ProductCard({product_name,image_url,description,price,rating,id}) {
   const navigate = useNavigate();
 
   return (
-    <Card style={{ width: '18rem',marginTop:'1%',height:"33rem"}} className="hover:shadow-2xl" onClick={() => {
-          navigate("/product/" + id)}}>
-      <Card.Img variant="top" src={image_url} />
-      <Card.Body>
-        <h4 className='font-bold text-lg'>{product_name} </h4>
-        <p className='font-bold text-lg'>Price: ${price}</p>
-        <p className='font-bold text-lg'>Rating:{rating} <i className="fa-regular fa-star"></i></p>
-        <Card.Text className='font-medium text-sm'>
-         Description: {description}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <div className='h-[500px] w-80 p-8 mt-4 shadow-2xl mr-4'  onClick={() => {
+    navigate("/product/" + id)}}>
+       <img src={image_url} className='w-full h-1/2'/>
+       <h4 className='text-xl font-bold mb-0'>{product_name}</h4>
+       <img src={Star3}  className='w-16 h-4'/>
+       <h4 className='text-sm mt-2'>{description}</h4>
+       <div className='flex mt-8'>
+        <h4 className='text-lg text-[#ccc]'>price:</h4>
+        <h4 className='text-lg ml-1'>${price}</h4>
+       </div>
+    </div>
+    
   );
 }
+
+// <Card style={{ width: '18rem',marginTop:'1%',height:"33rem"}} className="hover:shadow-2xl" onClick={() => {
+    //       navigate("/product/" + id)}}>
+    //   <Card.Img variant="top" src={image_url} />
+    //   <Card.Body>
+    //     <h4 className='font-bold text-lg'>{product_name} </h4>
+    //     <p className='font-bold text-lg'>Price: ${price}</p>
+    //     <p className='font-bold text-lg'>Rating:{rating} <i className="fa-regular fa-star"></i></p>
+    //     <Card.Text className='font-medium text-sm'>
+    //      Description: {description}
+    //     </Card.Text>
+    //   </Card.Body>
+    // </Card>
