9a04457cbd934c21dafb7f328824c19341c5795c
Sherif Okunade <o.d.alsherif@gmail.com>
2022-11-20 11:51:44 +0100

    Update Cart.jsx

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -31,7 +31,7 @@ const Cart = () => {
                 }
             </div>}
             <div className="checkout flex w-4/5 md:w-3/5 justify-between mt-3 border-t-2 border-black border-solid">
-              <span className="text-lg font-bold mt-4"> Total:$<span className="border-solid border-2 border-black px-3 py-2 mt-4">
+              <span className="text-lg font-bold mt-4"> Total:₦<span className="border-solid border-2 border-black px-3 py-2 mt-4">
                 {cart.length>0? checkoutAmount: 0 }
                 </span> </span>
              { checkoutAmount<0.1?
